61aa740a83a0b7cff0ec64671a759f536f856548
BitBlasters <scottmitchell@mac.com>
2025-04-22 08:09:49 -0600

    Fixed SQL statement from returning the deleted item.

--- server.js
+++ server.js
@@ -105,14 +105,13 @@ app.post("/api/flavors", async (req, res) => {
 app.delete("/api/flavors/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   try {
-    const result = await client.query(
-      "DELETE FROM flavors WHERE id = $1 RETURNING *",
-      [id]
-    );
-    if (result.rows.length === 0) {
-      return res.status(404).send("Flavor not found");
-    }
-    res.json(result.rows[0]);
+    const result = await client.query("DELETE FROM flavors WHERE id = $1", [
+      id,
+    ]);
+    // if (result.rows.length === 0) {
+    //   return res.status(404).send("Flavor not found");
+    // }
+    // res.json(result.rows[0]);
   } catch (err) {
     console.error("Error deleting data:", err);
     res.status(500).send("Internal Server Error");
